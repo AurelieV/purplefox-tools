6da089b280bb0d75625da63843baf77ac290c435
Aurélie VIOLETTE <aurelie.violette@purple-fox.fr>
2021-12-18 18:34:45 +0100

    Add link and footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <footer class="px-1 py-2 text-xs text-center text-gray-700">
+        <p>
+            This is a <a class="pf-link" href="https://purple-fox.fr/">PurpleFox</a> tool, developed by PurpleOrwel aka
+            Aurélie VIOLETTE.
+        </p>
+        <nav>
+            <ul class="flex justify-center gap-4">
+                <li>
+                    <a class="flex items-center pf-link" href="https://bit.ly/3yLzyAb"
+                        ><Pf_Icon inline icon="mdi:twitter" class="mr-1" />Twitter</a
+                    >
+                </li>
+                <li>
+                    <a class="flex items-center pf-link" href="https://bit.ly/3sfmS3p">
+                        <Pf_Icon inline icon="mdi:github" class="mr-1" />GitHub
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </footer>
+</template>
