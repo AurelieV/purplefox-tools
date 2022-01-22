0a5aa2fce12d8b4dbc31c30dd3d21337c0bfcb2b
Aurélie VIOLETTE <orwel.violette@gmail.com>
2022-01-22 15:45:09 +0100

    Add an init for responsive plugin

--- src/plugins/responsive.js
+++ src/plugins/responsive.js
@@ -1,28 +1,28 @@
 import { reactive } from "vue";
 
-const mediaQueryLists = [
-    { breakpoint: "sm", mediaQueryList: window.matchMedia("(min-width: 640px)") },
-    { breakpoint: "md", mediaQueryList: window.matchMedia("(min-width: 768px)") },
-    { breakpoint: "lg", mediaQueryList: window.matchMedia("(min-width: 1024px)") },
-    { breakpoint: "xl", mediaQueryList: window.matchMedia("(min-width: 1280px)") },
-    { breakpoint: "2xl", mediaQueryList: window.matchMedia("(min-width: 1536px)") },
-];
-
 const breakpoints = reactive({});
-
-mediaQueryLists.forEach(({ breakpoint, mediaQueryList }) => {
-    function onMediaQueryListChange(evt) {
-        breakpoints[breakpoint] = evt.matches;
-    }
-
-    mediaQueryList.addListener(onMediaQueryListChange);
-
-    // We define the initial state because mediaQuery listeners will not be called until one value changes
-    breakpoints[breakpoint] = mediaQueryList.matches;
-});
 
 export default function install(app) {
     app.config.globalProperties.$responsive = reactive({
         breakpoints,
+        init() {
+            const mediaQueryLists = [
+                { breakpoint: "sm", mediaQueryList: window.matchMedia("(min-width: 640px)") },
+                { breakpoint: "md", mediaQueryList: window.matchMedia("(min-width: 768px)") },
+                { breakpoint: "lg", mediaQueryList: window.matchMedia("(min-width: 1024px)") },
+                { breakpoint: "xl", mediaQueryList: window.matchMedia("(min-width: 1280px)") },
+                { breakpoint: "2xl", mediaQueryList: window.matchMedia("(min-width: 1536px)") },
+            ];
+            mediaQueryLists.forEach(({ breakpoint, mediaQueryList }) => {
+                function onMediaQueryListChange(evt) {
+                    breakpoints[breakpoint] = evt.matches;
+                }
+
+                mediaQueryList.addListener(onMediaQueryListChange);
+
+                // We define the initial state because mediaQuery listeners will not be called until one value changes
+                breakpoints[breakpoint] = mediaQueryList.matches;
+            });
+        },
     });
 }
