70da27ef90179b8f5e7062a7ccf6d7605985ba12
Aurélie VIOLETTE <orwel.violette@gmail.com>
2022-02-12 15:54:38 +0100

    Add onprimary button

---
+++ src/components/Icon.vue
@@ -0,0 +1,14 @@
+<template>
+    <!-- We need an additional span to handle SSR. Maybe punish later for it -->
+    <span>
+        <Icon v-bind="$attrs"></Icon>
+    </span>
+</template>
+
+<script>
+import { Icon } from "@iconify/vue";
+export default {
+    components: { Icon },
+    inheritAttrs: false,
+};
+</script>
