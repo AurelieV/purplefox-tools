4b28903f97d01ad97a0b1ef3796f367c2e41eb7e
Aurélie VIOLETTE <orwel.violette@gmail.com>
2022-08-27 08:04:28 +0200

    No prerendering

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,14 +1,16 @@
 <template>
     <!-- We need an additional span to handle SSR. Maybe punish later for it -->
-    <span class="flex items-center justify-center">
+    <span v-if="!noPrerender" class="flex items-center justify-center">
         <Icon v-bind="$attrs"></Icon>
     </span>
+    <Icon v-else v-bind="$attrs"></Icon>
 </template>
 
 <script>
 import { Icon } from "@iconify/vue";
 export default {
     components: { Icon },
+    inject: { pfNoPrerender: { default: false } },
     inheritAttrs: false,
 };
 </script>
